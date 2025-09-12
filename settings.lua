2d3094e457d06c4a6b1e8e29729e5be5d9e30c3d
fainda <fainda@protonmail.com>
2025-09-12 21:45:36 +0200

    update settings

---
+++ settings.lua
@@ -0,0 +1,38 @@
+data:extend({
+    --[[
+        the order of settings is categorized as follows:
+        0x: ubiquitous (enviroment) settings
+        1x: basic (gameplay) settings
+        2x: advanced (gameplay) settings
+    ]]
+    --enviroment settings
+    {
+        type = "bool-setting",
+        name = "debug-mode",
+        setting_type = "runtime-global",
+        default_value = false,
+        order = "00"
+    },
+    --advanced settings
+    {
+        type = "string-setting",
+        name = "mnemonic",
+        setting_type = "runtime-global",
+        default_value = "international",
+        allowed_values = {
+            "international",
+            "german"
+        },
+        order = "20"
+    },
+    {
+        type = "int-setting",
+        name = "cycle-gap",
+        setting_type = "runtime-global",
+        default_value = 10,
+        minimum_value = 0,
+        maximum_value = 60,
+        order = "20"
+    }
+})
+
